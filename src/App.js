bd1cd937c8c2b26b27ba2870e960393040f02989
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-06-15 15:26:16 +0600

    feature added It is now possible to add an item to the center of any two elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import useAlphabets from "./hooks/useAlphabets";
 
@@ -19,6 +19,8 @@ function App() {
     const [action, setAction] = useState(null);
     const [rsh, setRSH] = useState(null);
 
+    const dragOverItem = useRef(); // To store current item insert position
+
     // To handle RSH Integer Input
     const handlerRSH = () => {
         const rshInteger = window.prompt("What should be the RSH Integer?");
@@ -33,13 +35,25 @@ function App() {
     // onDragStart we setDragData and get the current drag data.
     // useState instead of e.dataTransfer so we can transfer more data
     const handleDragStart = (e, item, value, type) => {
-        setDragData({ id: droppedData.length + 1, item, value, type });
+        setDragData({ id: droppedData.length, item, value, type });
+    };
+
+    //Tracking item insert position
+    const dragEnter = (e, position) => {
+        dragOverItem.current = position;
+        console.log(dragOverItem.current);
     };
 
     //Inserting Items to droppedData based on onDrop event
     const handleDrop = (e) => {
-        const data = [...droppedData, dragData];
-        setDroppedData(data);
+        const droppedItems = [...droppedData];
+        if (dragOverItem.current) {
+            droppedItems.splice(dragOverItem.current, 0, dragData);
+        } else {
+            droppedItems.push(dragData);
+        }
+        dragOverItem.current = null;
+        setDroppedData(droppedItems);
     };
 
     //To Remove Dropped item
@@ -72,6 +86,7 @@ function App() {
                     <div
                         key={alphabet._id}
                         draggable
+                        // onDragEnd={(e) => console.log("onDragEnd")}
                         className="alphabet"
                         onDragStart={(e) =>
                             handleDragStart(
@@ -87,7 +102,7 @@ function App() {
                     </div>
                 ))}
             </div>
-            
+
             {/* operator section  */}
             <div className="operators-container">
                 <div className="operators-box">
@@ -131,8 +146,12 @@ function App() {
                 onDrop={(e) => handleDrop(e)}
                 className="expression-container"
             >
-                {droppedData.map((data) => (
-                    <div key={data.id} className={`item ${data.type}`}>
+                {droppedData.map((data, index) => (
+                    <div
+                        key={index}
+                        onDragEnter={(e) => dragEnter(e, index)}
+                        className={`item ${data.type}`}
+                    >
                         {data.item}
                         <button
                             onClick={() => removeDroppedItemHandler(data.id)}
